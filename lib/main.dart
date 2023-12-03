443bbe32c2e335bec4bc7ce9dd7f3b21649131b4
SHAKIR <shakirsjd@gmail.com>
2023-12-03 17:05:33 +0530

    done 1 thing

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,13 @@ void main() {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('I am rich'),
-          backgroundColor: Colors.amber,
+          backgroundColor: Colors.black,
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://media.npr.org/assets/img/2013/03/11/istock-4306066-baby_custom-aee07650b312c1f240125b669c2aa92a7d36e73b-s1100-c50.jpg'), // Replace with a valid image URL
+          ),
         ),
       ),
     ),
